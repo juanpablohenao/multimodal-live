c149718dab4980dd4312d5a8038bc34db0009630
juan pablo <juanpablohenao@casamaestra.net>
2025-02-07 11:55:48 -0500

    dotenv

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ from google.genai import types
 import os
 from dotenv import load_dotenv
 
-print("Hello world2")
-app = Flask(__name__)
-CORS(app)
-# GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
-GOOGLE_API_KEY="AIzaSyAMveklntNsU81Msld_yXfKt0jl8gTJ6zY"
 load_dotenv()
+
+print("Hello world3")
+app = Flask(__name__)
+CORS(app) 
+# GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
+# GOOGLE_API_KEY="AIzaSyAMveklntNsU81Msld_yXfKt0jl8gTJ6zY"
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+print(GOOGLE_API_KEY)
 client = genai.Client(api_key=GOOGLE_API_KEY)
 MODEL_ID = "gemini-2.0-flash-exp" # @param ["gemini-1.5-flash-8b","gemini-1.5-flash-002","gemini-1.5-pro-002","gemini-2.0-flash-exp"] {"allow-input":true}
 
@@ -43,4 +45,4 @@ def create_customer():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
